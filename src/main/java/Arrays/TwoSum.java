71b4afee6c159e26e4094aa2d7ed67a007d734a9
danielwalbridge <danielwalbridge@gmail.com>
2022-09-15 18:26:13 -0400

    TwoSum pointer method added

--- src/main/java/Arrays/TwoSum.java
+++ src/main/java/Arrays/TwoSum.java
@@ -8,9 +8,11 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
  */
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
+
 
     public int[] twoSum(int[] nums, int target) {
 
@@ -22,17 +24,48 @@ public class TwoSum {
 
             if (map.containsKey(nums[i])) {
                 return new int[]{map.get(nums[i]), i};
-            }
-            else {
+            } else {
                 int key = target- nums[i];
                 map.put(key, i);
             }
         }
         return new int[]{};
     }
-}
 
 /*
 O(n) time : n = size of input array.
 O(m) space : m = size of map created.
  */
+
+    public int[] twoSumsPointers(int[] nums, int target) {
+
+        Arrays.sort(nums);
+
+        int lowPointer= 0;
+        int highPointer = nums.length-1;
+
+        for (int i =0; i<nums.length;i++) {
+
+            while (nums[highPointer] > target) {
+                highPointer--;
+            }
+            if (nums[lowPointer] + nums[highPointer] == target) {
+                return new int[]{lowPointer,highPointer};
+            }
+            else {
+                lowPointer++;
+                highPointer--;
+            }
+        }
+        return new int[]{};
+    }
+
+    /*
+
+    This method does add more time as we first sort the array.
+    This brings the time up to O(n log n)
+    however, it saves on space, as we no longer needed to create a DS.
+    This brings the space down to O(1)
+     */
+
+}
