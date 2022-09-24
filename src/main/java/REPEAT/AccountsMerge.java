de7c76c7abbef96a919f516c1572a8e532255ab6
danielwalbridge <danielwalbridge@gmail.com>
2022-09-24 13:30:35 -0400

    AccountsMerge

---
+++ src/main/java/REPEAT/AccountsMerge.java
@@ -0,0 +1,18 @@
+package REPEAT;
+
+import java.util.List;
+
+public class AccountsMerge {
+
+    /*
+    I have some notes in my notion on this, but I was not able to get it.
+    Was trying to use HashMaps, and did not realize until closer to the end of my 30 mins that I need to use some type of graph.
+    - I'm not well practised in graphs, so will need to come back to this problem at a later time and give it another go
+
+     */
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+
+  return accounts;
+    }
+}
