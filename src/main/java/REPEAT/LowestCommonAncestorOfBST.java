3ecddf1adac3c95b2611c9392d12a7621f1c5609
danielwalbridge <danielwalbridge@gmail.com>
2022-11-18 13:55:24 -0500

    LowestCommonAncestorOfBST problem

--- src/main/java/REPEAT/LowestCommonAncestorOfBST.java
+++ src/main/java/REPEAT/LowestCommonAncestorOfBST.java
@@ -21,7 +21,36 @@ Output: 6
 Explanation: The LCA of nodes 2 and 8 is 6.
 
 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the BST
  */
 
+import Trees.TreeNode;
+
 public class LowestCommonAncestorOfBST {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        // traverse left.
+        if (p.val < root.val && q.val < root.val) {
+            lowestCommonAncestor(root.left, p,q);
+        }
+
+        // traverse right.
+        if (p.val > root.val && q.val > root.val) {
+            lowestCommonAncestor(root.right, p,q);
+        }
+
+        return root;
+    }
+
+    // We will return the root if both are not on the left or right. If one is on the left, and one is on the right, or one is equal...Then we know we have foudn the LCA
+
+    //  O(n) + recursive space, can be done iteratively for O(1) space
 }
