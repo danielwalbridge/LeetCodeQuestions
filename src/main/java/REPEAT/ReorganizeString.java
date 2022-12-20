1522b49d582a486efe265ddeb192927d4d381bc4
danielwalbridge <danielwalbridge@gmail.com>
2022-12-20 10:28:05 -0500

    ReorganizeString

--- src/main/java/REPEAT/ReorganizeString.java
+++ src/main/java/REPEAT/ReorganizeString.java
@@ -23,11 +23,34 @@ Constraints:
 s consists of lowercase English letters.
  */
 
+import java.util.Arrays;
+
 public class ReorganizeString {
 
     public String reorganizeString(String s) {
 
-        return "";
+        if (s.length() <=1) return s;
 
+        char[] chars = s.toCharArray();
+
+        int j = 0;
+        for (int i = 0; i < chars.length; i++) {
+            j= i+1;
+            int firstJIndex =j;
+            if (j < chars.length) {
+                int firstJIndexChar =chars[j];
+                while (j < chars.length && chars[i] == chars[j] ) {
+                    j++;
+                }
+                chars[firstJIndex] = chars[j];
+                chars[j] = chars[firstJIndexChar];
+            }
+        }
+
+        if (chars[chars.length-1] == chars[chars.length-2]) {
+            return "";
+        }
+        return Arrays.toString(chars);
     }
+    // first attempt and rusty after not doing kota for a bit
 }
